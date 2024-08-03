c45957bee22f48be74127aa7246e0efe0a29a829
Karan <email@karanshergill.com>
2024-08-03 16:04:54 +0530

    install styled components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-moderate-violet': 'hsl(263, 55%, 52%)',
+        'color-very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'color-very-dark-blackish-blue': 'hsl(219, 29%, 14%)',
+        'color-white': 'hsl(0, 0%, 100%)',
+        'color-light-gray': 'hsl(0, 0%, 81%)',
+        'color-light-grayish-blue': 'hsl(210, 46%, 95%)'
+      },
+      fontFamily: {
+        'Barlow Semi Condensed': ['Barlow Semi Condensed']
+      }
+    },
   },
   plugins: [],
 }
